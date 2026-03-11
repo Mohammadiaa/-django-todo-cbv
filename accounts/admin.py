7d3dfce81376bf2861a385f74fd29eb7198aa984
Mohammadi <amohammadia0086@gmail.com>
2026-03-11 19:33:47 +0330

    add Profile admin configuration
    Register Profile model in Django admin and configure ProfileAdmin.
    
    - add list_display for user, first_name, last_name, created_at
    - enable search by user email and names
    - add filters for created_at and updated_at
    - mark created_at and updated_at as readonly
    - optimize queries with list_select_related

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 # from django.contrib import admin
 # from django.contrib.auth.admin import UserAdmin
 # from .models import User
+from django.contrib import admin
+from .models import Profile
 # # Register your models here.
 
 # class CustomUserAdmin(UserAdmin):
@@ -24,6 +26,12 @@
 #         }),
 #     )
 
-    
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "first_name", "last_name", "created_at")
+    list_select_related = ("user",)
+    search_fields = ("user__email", "first_name", "last_name")
+    list_filter = ("created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
 
 # admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile, ProfileAdmin)
