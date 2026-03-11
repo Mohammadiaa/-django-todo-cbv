fce81eb1763ac7907d843f9e17a5c58e97e0923e
Mohammadi <amohammadia0086@gmail.com>
2026-03-11 16:53:11 +0330

    Configure Django admin for custom User model
    Added CustomUserAdmin with list display, filters, search, and fieldsets.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active","updated_date")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "first_name", "last_name", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+    
+
+admin.site.register(User, CustomUserAdmin)
