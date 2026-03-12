d0abd765d86b6f09be67fe76833549efb143c734
Mohammadi <amohammadia0086@gmail.com>
2026-03-12 20:31:57 +0330

    Refactor UI and template structure
    - Move login template to templates/registration/ for Django auth convention
    - Add logged_out.html template
    - Add custom stylesheet (css/style.css)
    - Refactor base.html navbar with custom classes
    - Replace inline styles with reusable CSS classes
    - Improve project structure

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import TaskListView,CreateTaskView,EditTaskView,DeleteTaskView
+from .views import TaskListView,CreateTaskView,EditTaskView,DeleteTaskView,ToggleTaskView
 
 app_name = "tasks"
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path("",TaskListView.as_view(), name="task_list"),
     path("create/",CreateTaskView.as_view(), name="create_task"),
     path("edit/<int:pk>/",EditTaskView.as_view(), name="edit_task"),
-    path("delete/<int:pk>/",DeleteTaskView.as_view(), name="delete_task")
+    path("delete/<int:pk>/",DeleteTaskView.as_view(), name="delete_task"),
+     path("toggle/<int:pk>/",ToggleTaskView.as_view(), name="task_toggle"), 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
+from .models import Task
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
 # Create your views here.
-
-class TaskListView(TemplateView):
+class TaskListView(LoginRequiredMixin,ListView):
+     model = Task
      template_name = "tasks/task_list.html"
+     context_object_name = "tasks"
 
+     def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+     
+class ToggleTaskView(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk, user=request.user)
+        task.is_done = not task.is_done
+        task.save()
+        return redirect('tasks:task_list')
 class CreateTaskView(TemplateView):
      template_name = "tasks/task_form.html"    
 
