fc5aa729115ce8cdd5924539e7383341fd7c5d6e
Mohammadi <amohammadia0086@gmail.com>
2026-03-12 14:09:43 +0330

    Include tasks and accounts URLs in core urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,19 +16,17 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    
-
-    # path("",TemplateView.as_view(template_name="tasks/task_list.html"),name="home1"),
-
-     
-    # path("profile/",TemplateView.as_view(template_name="profile/profile.html"),name="home2"),
+    path("tasks/", include("tasks.urls")),
+    path("accounts/", include("accounts.urls")),
+ 
 ]
 
 
