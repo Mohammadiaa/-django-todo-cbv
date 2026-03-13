d3307e8e76536466f2cc5af93c230aab49803c02
Mohammadi <amohammadia0086@gmail.com>
2026-03-13 17:51:30 +0330

    feat(tasks): add task edit functionality and dynamic form title
    - Implemented EditTaskView using Django's UpdateView.
    - Integrated LoginRequiredMixin to restrict access.
    - Updated task_form.html to display "Edit Task" or "New Task" dynamically based on the context.

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView,CreateView,DetailView,DeleteView
+from django.views.generic import TemplateView,ListView,CreateView,DetailView,DeleteView,UpdateView
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect
@@ -31,13 +31,17 @@ class CreateTaskView(LoginRequiredMixin,CreateView):
           form.instance.user = self.request.user
           return super().form_valid(form)
          
+class EditTaskView(LoginRequiredMixin,UpdateView):
+     model = Task
+     form_class = TaskForm  
+     template_name = "tasks/task_form.html"    
+     success_url = "/tasks/"
 class TaskDetailView(LoginRequiredMixin,DetailView):
      model = Task
      template_name = 'tasks/task_detail.html'
      context_object_name = 'task'
 
-class EditTaskView(TemplateView):
-     template_name = "tasks/task_form.html" 
+
 
 class DeleteTaskView(LoginRequiredMixin,DeleteView):
      model = Task
